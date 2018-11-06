a7489c82a7e7678c5bf246c1a85bebd471f99183
GROVES <S6076383@live.tees.ac.uk>
2018-11-06 14:27:04 +0000

    QUick commit to save progress before merge

---
+++ Assets/Scripts/Prototype/Hazard/Hazard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject hazard;
+    void HazardDetection()
+    {
+        Collider2D[] hitCollider = Physics2D.OverlapCircleAll(hazard.transform.position, 1.0f, 1 << 8);
+
+        if(hitCollider.Length != 0)
+        {
+            for (int i = 0; i < hitCollider.Length; i++)
+            {
+                TankManager tank = hitCollider[i].transform.root.GetComponent<TankManager>();
+                if (tank != null)
+                {
+                    
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        HazardDetection();
+    }
+}
