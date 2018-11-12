6bed4de0f28c0378ce780e1c49b1d6f5014185d7
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2018-11-12 16:59:40 +0000

    Quick trap fix

--- Assets/Scripts/Prototype/Hazard/Hazard.cs
+++ Assets/Scripts/Prototype/Hazard/Hazard.cs
@@ -6,38 +6,17 @@ public class Hazard : MonoBehaviour
 {
     public float damage = 10;
 
-        //if(hitCollider.Length != 0)
-        //{
-        //    for (int i = 0; i < hitCollider.Length; i++)
-        //    {
-                
-        //    }
-        //}
-    }
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         TankManager tank = collision.transform.root.GetComponent<TankManager>();
         if (tank != null)
         {
-            Vector2 direction = (hazard.transform.position - tank.transform.position).normalized;
+            Vector2 direction = (gameObject.transform.position - tank.transform.position).normalized;
             DamageData data = new DamageData(damage, direction, tank.transform.position);
             tank.OnHit(data);
+            OnTankHit(tank);
         }
     }
 
     public virtual void OnTankHit(TankManager tank) { }
-
-    private void Update()
-    {
-        HazardDetection();
-    }
 }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        TankManager tank = collision.transform.root.GetComponent<TankManager>();
-            Vector2 direction = (gameObject.transform.position - tank.transform.position).normalized;
-            DamageData data = new DamageData(damage, direction, tank.transform.position);
-
-    public virtual void OnTankHit(TankManager tank) { }
