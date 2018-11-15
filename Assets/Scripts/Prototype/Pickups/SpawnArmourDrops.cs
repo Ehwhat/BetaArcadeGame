46cb343b74ef9457b3529e1d2e5eb5061715f7c3
ackar89124 <35962691+ackar89124@users.noreply.github.com>
2018-11-15 18:42:33 +0000

    Added armour block spawners
    Armour block spawners add armour pickups to the map at set times based on the number of players present (currently just a public int), these times and areas can be modified.

---
+++ Assets/Scripts/Prototype/Pickups/SpawnArmourDrops.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArmourDrops : MonoBehaviour {
+
+    public GameObject part;
+    public int numberOfPlayers = 4;
+    public Vector2 area = new Vector2 (3.0f, 3.0f);
+    public int timerSet = 960;
+    private int timer;
+
+    // Use this for initialization
+    void Start () {
+        timerSet /= numberOfPlayers;
+        timer = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer--;
+        if(timer <= 0)
+        {
+            timer = timerSet;
+            float tempX = Random.Range(area.x, -area.x);
+            float tempY = Random.Range(area.y, -area.y);
+            Vector3 tempPos = new Vector3 (Random.Range(area.x, -area.x), Random.Range(area.y, -area.y), 0);
+            GameObject tempPart = Instantiate(part);
+            tempPart.transform.SetParent(this.transform);
+            tempPart.transform.localPosition = tempPos;
+        }
+	}
+}
