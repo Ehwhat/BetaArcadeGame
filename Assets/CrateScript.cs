94b203e2aa7b73da3f925e0d430221bf5cc82158
ackar89124 <35962691+ackar89124@users.noreply.github.com>
2018-12-06 21:05:22 +0000

    Added crates and modified armour spawners in progress
    Crates do not get damaged on hit yet and the game manager needs connecting to the armour spawner and the crates

---
+++ Assets/CrateScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CrateScript : MonoBehaviour, IDamageable {
+
+    public GameManager instance;
+    public GameObject part;
+    public float area = 1.5f;
+    public int partsPerPlayer = 3;
+    public int numberOfPlayers = 4;
+    public float health = 100;
+
+    public void OnHit(DamageData damage)
+    {
+        health -= damage.damage;
+        if (health <= 0)
+        {
+            DestroyCrate();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        numberOfPlayers = instance.players.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    void PlaceArmour()
+    {
+        Vector3 tempPos = new Vector3(Random.Range(area, -area), Random.Range(area, -area), 0);
+        GameObject tempPart = Instantiate(part);
+        tempPart.transform.parent = transform;
+        tempPart.transform.localPosition = tempPos;
+    }
+
+    void DestroyCrate()
+    {
+        for (int i = 0; i < partsPerPlayer * numberOfPlayers; i++)
+        {
+            PlaceArmour();
+        }
+        transform.DetachChildren();
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Prototype/Pickups/SpawnArmourDrops.cs
+++ Assets/Scripts/Prototype/Pickups/SpawnArmourDrops.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class SpawnArmourDrops : MonoBehaviour {
 
+    public GameManager instance;
     public GameObject part;
     public int numberOfPlayers = 4;
     public Vector2 area = new Vector2 (3.0f, 3.0f);
     public int timerSet = 960;
     private int timer;
+    public int startingPartsPerPlayer = 1;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        numberOfPlayers = instance.players.Length;
         timerSet /= numberOfPlayers;
         timer = 1;
+        for (int i = 0; i < startingPartsPerPlayer * numberOfPlayers; i++)
+        {
+            PlaceArmour();
+        }
 	}
 	
 	// Update is called once per frame
@@ -22,12 +30,17 @@ public class SpawnArmourDrops : MonoBehaviour {
         if(timer <= 0)
         {
             timer = timerSet;
-            float tempX = Random.Range(area.x, -area.x);
-            float tempY = Random.Range(area.y, -area.y);
-            Vector3 tempPos = new Vector3 (Random.Range(area.x, -area.x), Random.Range(area.y, -area.y), 0);
-            GameObject tempPart = Instantiate(part);
-            tempPart.transform.SetParent(this.transform);
-            tempPart.transform.localPosition = tempPos;
+            PlaceArmour();
         }
 	}
+
+    void PlaceArmour()
+    {
+        float tempX = Random.Range(area.x, -area.x);
+        float tempY = Random.Range(area.y, -area.y);
+        Vector3 tempPos = new Vector3(Random.Range(area.x, -area.x), Random.Range(area.y, -area.y), 0);
+        GameObject tempPart = Instantiate(part);
+        tempPart.transform.SetParent(this.transform);
+        tempPart.transform.localPosition = tempPos;
+    }
 }
